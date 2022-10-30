1eba07a2cfecee754770dab55e543b6c6a099b2b
주정준 <65483521+jun1485@users.noreply.github.com>
2022-10-30 22:08:26 +0900

    Create emit.vue

---
+++ emit.vue
@@ -0,0 +1,27 @@
+// home.vue
+
+<template>
+  <div>
+    <h1>Welcome to Home</h1>
+    <inputField :title="title" />
+  </div>
+</template>
+<script>
+import inputField from '@/components/inputField.vue'
+export default {
+  data() {
+    return {
+      title: ""
+    }
+  },
+  components: {
+    inputField
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+// inputField
