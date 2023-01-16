480f2d7c14bedd21544dff1aa65f3776da799598
주정준 <65483521+jun1485@users.noreply.github.com>
2023-01-16 21:18:26 +0900

    Update and rename html.vue to template.vue

--- html.vue
+++
@@ -1,2 +0,0 @@
-<p>텍스트 보간법 사용: {{ rawHtml }}</p>
-<p>v-html 디렉티브 사용: <span v-html="rawHtml"></span></p>
---
+++ template.vue
@@ -0,0 +1,6 @@
+<p>텍스트 보간법 사용: {{ rawHtml }}</p>
+<p>v-html 디렉티브 사용: <span v-html="rawHtml"></span></p>
+
+
+// 이중 중괄호는 HTML 속성 내에서 사용 불가능하기 때문에 v-bind 사용
+<div :id="dynamicId"></div>
