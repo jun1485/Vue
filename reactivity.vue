7a4e56b000eb4883bd87ffeb08374c252b766b26
주정준 <65483521+jun1485@users.noreply.github.com>
2023-01-30 09:07:41 +0900

    Update reactivity.vue

--- reactivity.vue
+++ reactivity.vue
@@ -36,7 +36,8 @@ export default {
 export default {
   methods: {
     increment: () => {
-      //  여기서 `this`에 접근할 수 없음
+      //  화살표 함수는 vue가 this를 컴포넌트 인스턴스로 바인딩하는 것을 방지하기 떄문에
+      //  여기서 `this`에 접근할 수 없음,
     }
   }
 }
