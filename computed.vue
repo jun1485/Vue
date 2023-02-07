c1e45ff8c5c89b9433f8385c0eec250abb9b1625
주정준 <65483521+jun1485@users.noreply.github.com>
2023-02-07 09:09:07 +0900

    Create computed.vue

---
+++ computed.vue
@@ -0,0 +1,9 @@
+<p>{{ calculateBooksMessage() }}</p>
+
+// 컴포넌트 내에서
+// 계산된 속성은 의존된 반응형을 기반으로 캐시.
+methods: {
+  calculateBooksMessage() {
+    return this.author.books.length > 0 ? 'Yes' : 'No'
+  }
+}
