1e192513296aa4d6f4460dd8dd6861c3469cc3e9
주정준 <65483521+jun1485@users.noreply.github.com>
2023-02-09 09:01:32 +0900

    Update computed.vue

--- computed.vue
+++ computed.vue
@@ -15,3 +15,28 @@ computed: {
     return Date.now()
   }
 }
+
+
+// computed 속성은 기본적으로 getter 전용.
+// "수정 가능한" computed가 필요한 경우, getter와 setter를 모두 제공하여 속성을 만들 수 있음.
+export default {
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe'
+    }
+  },
+  computed: {
+    fullName: {
+      // getter
+      get() {
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter
+      set(newValue) {
+        // 참고: 분해 할당 문법을 사용함.
+        [this.firstName, this.lastName] = newValue.split(' ')
+      }
+    }
+  }
+}
