8eb4389ea08511f4741071bfb6bf116ca2168c46
주정준 <65483521+jun1485@users.noreply.github.com>
2023-02-11 09:10:31 +0900

    Create classBinding.vue

---
+++ classBinding.vue
@@ -0,0 +1,18 @@
+// 객체로 바인딩
+<div :class="{ active: isActive }"></div>
+
+
+<div
+  class="static"
+  :class="{ active: isActive, 'text-danger': hasError }"
+>
+</div>
+
+data() {
+  return {
+    isActive: true,
+    hasError: false
+  }
+}
+// 최종적으로 아래처럼 렌더링됨.
+<div class="static active"></div>
