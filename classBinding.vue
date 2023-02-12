1a83b560c899740297d78d8280da7d9a7b35ed90
주정준 <65483521+jun1485@users.noreply.github.com>
2023-02-12 23:03:05 +0900

    Update classBinding.vue

--- classBinding.vue
+++ classBinding.vue
@@ -16,3 +16,16 @@ data() {
 }
 // 최종적으로 아래처럼 렌더링됨.
 <div class="static active"></div>
+
+
+// 바인딩된 객체는 인라인일 필요가 없다.
+<div :class="classObject"></div>
+
+data() {
+  return {
+    classObject: {
+      active: true,
+      'text-danger': false
+    }
+  }
+}
