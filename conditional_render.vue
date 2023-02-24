73ec8a4856b502468c375114b15f90a767cce26e
주정준 <65483521+jun1485@users.noreply.github.com>
2023-02-24 16:25:28 +0900

    Update conditional_render.vue

--- conditional_render.vue
+++ conditional_render.vue
@@ -1,3 +1,7 @@
 // v-if, 디렉티브 표현식이 truthy일 때만 렌더링
 <h1 v-if="awesome">Vue는 정말 멋지죠!</h1>
 <h1 v-else>아닌가요? 😢</h1>
+
+
+// v-show, falsy에서도 항상 요소가 렌더링 되어 DOM에 남아있는 상태, display CSS속성만 변화한다.
+<h1 v-show="ok">안녕!</h1>
