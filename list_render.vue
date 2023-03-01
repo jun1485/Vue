453e712e38e88ad26fcf99f2f118f0ed56941aef
주정준 <65483521+jun1485@users.noreply.github.com>
2023-03-01 22:19:00 +0900

    Update list_render.vue

--- list_render.vue
+++ list_render.vue
@@ -21,3 +21,15 @@ data() {
     items: [{ message: 'Foo' }, { message: 'Bar' }]
   }
 }
+
+
+const parentMessage = 'Parent'
+const items = [
+  /* ... */
+]
+
+items.forEach((item, index) => {
+  // forEach의 콜백 함수 외부에 있는 `parentMessage`에 대한 접근 가능.
+  // 반면 `item`과 `index`는 콜백함수 내부에서만 접근 가능.
+  console.log(parentMessage, item.message, index)
+})
