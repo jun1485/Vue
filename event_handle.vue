e875ada79bcad60954b4e3d4d98271522ece5f53
주정준 <65483521+jun1485@users.noreply.github.com>
2023-03-07 09:08:54 +0900

    Update event_handle.vue

--- event_handle.vue
+++ event_handle.vue
@@ -7,3 +7,21 @@ data() {
     count: 0
   }
 }
+
+
+
+<!-- 클릭 이벤트 전파가 중지됩니다. -->
+<a @click.stop="doThis"></a>
+
+<!-- submit 이벤트가 더 이상 페이지 리로드하지 않습니다. -->
+<form @submit.prevent="onSubmit"></form>
+
+<!-- 수식어를 연결할 수 있습니다. -->
+<a @click.stop.prevent="doThat"></a>
+
+<!-- 이벤트에 핸들러 없이 수식어만 사용할 수 있습니다. -->
+<form @submit.prevent></form>
+
+<!-- event.target이 엘리먼트 자신일 경우에만 핸들러가 실행됩니다. -->
+<!-- 예를 들어 자식 엘리먼트에서 클릭 액션이 있으면 핸들러가 실행되지 않습니다. -->
+<div @click.self="doThat">...</div>
