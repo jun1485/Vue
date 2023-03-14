2a62612ce47d084e65dc6f0b4b4cfd50d1629dae
주정준 <65483521+jun1485@users.noreply.github.com>
2023-03-14 09:12:56 +0900

    Create component_event.vue

---
+++ component_event.vue
@@ -0,0 +1,12 @@
+// 사용자 정의 이벤트 발신
+<!-- MyComponent -->
+<button @click="$emit('someEvent')">클릭하기</button>
+
+// 컴포넌트 인스턴스에서 this.$emit()으로도 사용 가능
+export default {
+  methods: {
+    submit() {
+      this.$emit('someEvent')
+    }
+  }
+}
