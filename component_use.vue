40de3f57bc41de8e7dcc78c69a4d2b2e18071f5c
주정준 <65483521+jun1485@users.noreply.github.com>
2023-03-22 09:14:47 +0900

    Create component_use.vue

---
+++ component_use.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+  setup() {
+    const state = reactive({
+      message: 'Hello, World!'
+    });
+
+    return {
+      message: state.message
+    }
+  }
+}
+</script>
+
+
+// 다른 컴포넌트에서 사용
+<template>
+  <div>
+    <my-component />
+  </div>
+</template>
+
+<script>
+import MyComponent from './MyComponent.vue';
+
+export default {
+  components: {
+    'my-component': MyComponent
+  }
+}
+</script>
