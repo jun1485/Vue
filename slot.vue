cf2556806d749c0363bc2cdbe663f9b433f6845d
주정준 <65483521+jun1485@users.noreply.github.com>
2023-03-27 09:11:17 +0900

    Update slot.vue

--- slot.vue
+++ slot.vue
@@ -6,3 +6,10 @@
 <button class="fancy-btn">
   <slot></slot> <!-- 슬롯 아울렛 -->
 </button>
+
+
+// 여러 엘리먼트 또는 컴포넌트도 전달 가능.
+<FancyButton>
+  <span style="color:red">클릭하기!</span>
+  <AwesomeIcon name="plus" />
+</FancyButton>
