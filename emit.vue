9377dd5852c8c333417777822c15b87a389161e8
주정준 <65483521+jun1485@users.noreply.github.com>
2023-04-10 23:09:43 +0900

    Update emit.vue

--- emit.vue
+++ emit.vue
@@ -24,4 +24,20 @@ export default {
 
 </style>
 
-// inputField
+
+
+// 자식 컴포넌트
+<template>
+  <button @click="sendData">데이터 보내기</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    sendData() {
+      // 여러 파라미터를 담아 보내기
+      this.$emit("update", "안녕하세요", 123);
+    },
+  },
+};
+</script>
