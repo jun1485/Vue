c6d774ca11bfa20884260213b9d70b8864151ce1
주정준 <65483521+jun1485@users.noreply.github.com>
2023-04-14 09:05:15 +0900

    Create v-model.vue

---
+++ v-model.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input v-model="message" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const message = ref('');
+    return { message };
+  },
+};
+</script>
