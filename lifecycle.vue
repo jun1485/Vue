2a9c13f0a594e4ca7bf0e709e5580b8d9e2f1851
주정준 <65483521+jun1485@users.noreply.github.com>
2023-04-23 23:24:48 +0900

    Create lifecycle.vue

---
+++ lifecycle.vue
@@ -0,0 +1,28 @@
+import { ref, onMounted, onUpdated, onUnmounted } from 'vue';
+
+export default {
+  setup() {
+    const count = ref(0);
+
+    const increment = () => {
+      count.value++;
+    };
+
+    onMounted(() => {
+      console.log('컴포넌트가 마운트되었습니다.');
+    });
+
+    onUpdated(() => {
+      console.log('컴포넌트가 업데이트되었습니다.');
+    });
+
+    onUnmounted(() => {
+      console.log('컴포넌트가 마운트 해제되었습니다.');
+    });
+
+    return {
+      count,
+      increment
+    };
+  },
+};
