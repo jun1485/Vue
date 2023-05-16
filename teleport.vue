af8f5ceef8903713415ce276bb1656d547f7c5be
주정준 <65483521+jun1485@users.noreply.github.com>
2023-05-17 00:31:23 +0900

    Update teleport.vue

--- teleport.vue
+++ teleport.vue
@@ -5,6 +5,6 @@
 // 모달, 알림, 팝업 등의 UI요소를 body태그 아래 또는 다른 DOM 요소로 이동
 // z-index 스태킹 컨텍스트, overflow:hidden 등 CSS문제를 우회하고 싶을 때
 
-<teleport to="#end-of-body">
+<teleport to="#end-of-body">    // "to" prop을 이용하여 대상 요소(CSS 셀렉터, DOM 요소)를 지정.
   <div>I will be rendered at the end of body tag!</div>
 </teleport>
