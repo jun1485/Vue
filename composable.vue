0ca98e075c368240ce8f749c00cfd36eaeb4ec4c
주정준 <65483521+jun1485@users.noreply.github.com>
2023-05-20 23:04:38 +0900

    Update composable.vue

--- composable.vue
+++ composable.vue
@@ -22,3 +22,27 @@ export default function useApi(url) {
 
   return { data, error, fetchData }
 }
+
+
+// 마우스 이벤트를 추적하는 컴포저블 함수
+import { ref, onMounted, onUnmounted } from 'vue'
+
+export function useMousePosition() {
+  const x = ref(0)
+  const y = ref(0)
+
+  const updateMouse = e => {
+    x.value = e.pageX
+    y.value = e.pageY
+  }
+
+  onMounted(() => {
+    window.addEventListener('mousemove', updateMouse)
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', updateMouse)
+  })
+
+  return { x, y }
+}
