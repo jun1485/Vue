22b61fcaf416f29e6aa7fcc2d9e3af4ea4e16b59
주정준 <65483521+jun1485@users.noreply.github.com>
2023-06-27 01:16:11 +0900

    Create SvgIcon.vue

---
+++ SvgIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="Icon" :width="width" :height="height" :fill="fill" />
+</template>
+
+<script>
+import * as icons from 'assets/icons/index';
+import { ref, watchEffect } from "vue";
+
+export default {
+  props: {
+    name: String,
+    width: Number,
+    height: Number,
+    fill: String
+  },
+
+  setup(props) {
+    const Icon = ref(null);
+
+    watchEffect(() => {
+      Icon.value = icons[props.name];
+    });
+
+    return { Icon };
+  }
+};
+</script>
