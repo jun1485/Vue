2b3b8c3f7ac7e79770141ac5ae7abeb90918a8ca
주정준 <65483521+jun1485@users.noreply.github.com>
2023-08-24 23:41:09 +0900

    Create ref.vue

---
+++ ref.vue
@@ -0,0 +1,10 @@
+// ref는 단일 값에 대한 반응성을 만듦, 기본형 값들을 직접 추적 가능.
+import { ref } from 'vue';
+
+const count = ref(0); // 초기값 0
+
+// 템플릿에서 사용
+// {{ count.value }}
+
+// 스크립트에서 사용
+count.value++; // 값 증가
