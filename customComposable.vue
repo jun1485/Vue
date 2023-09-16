69d46ecc9b81068cdcdc927b0f94214b76e2e49a
주정준 <65483521+jun1485@users.noreply.github.com>
2023-09-16 23:46:16 +0900

    Create customComposable.vue

---
+++ customComposable.vue
@@ -0,0 +1,20 @@
+// useCounter.ts
+import { ref } from 'vue';
+
+export default function useCounter(initialValue = 0) {
+  const count = ref(initialValue);
+  
+  const increment = () => {
+    count.value++;
+  };
+
+  const decrement = () => {
+    count.value--;
+  };
+
+  return {
+    count,
+    increment,
+    decrement
+  };
+}
