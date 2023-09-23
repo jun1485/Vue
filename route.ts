627e6f36e29184ae2ba797f04a4f36940c4e89aa
주정준 <65483521+jun1485@users.noreply.github.com>
2023-09-23 23:12:40 +0900

    Update route.ts

--- route.ts
+++ route.ts
@@ -10,8 +10,17 @@ const routes: RouteRecordRaw[] = [
     component: UserProfile,
     props: route => ({ username: route.params.username })
   },
+  {
+    path: '/product/:id',
+    name: 'ProductDetail',
+    component: ProductDetail,
+    props: route => ({ id: Number(route.params.id) })
+  }
+];
 
-  const router = createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes
-]
+});
+
+export default router;
