f8d377bebdfeba65bbb59335f57936e85ea44021
주정준 <65483521+jun1485@users.noreply.github.com>
2023-09-24 23:01:52 +0900

    Update route.ts

--- route.ts
+++ route.ts
@@ -4,6 +4,8 @@ import UserProfile from '../views/UserProfile.vue';
 import ProductDetail from '../views/ProductDetail.vue';
 
 const routes: RouteRecordRaw[] = [
+  // :username과 :id는 동적 세그먼트.
+  // 이 값들은 라우트가 변경될 때마다 해당 컴포넌트로 전달됨.
   {
     path: '/user/:username',
     name: 'UserProfile',
@@ -24,3 +26,6 @@ const router = createRouter({
 });
 
 export default router;
+
+
+// 동적 세그먼트 사용
