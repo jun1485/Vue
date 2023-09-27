f719bd7768ee86cab55ea693caacd682d41455d4
주정준 <65483521+jun1485@users.noreply.github.com>
2023-09-28 08:53:14 +0900

    Create nestedRoute.vue

---
+++ nestedRoute.vue
@@ -0,0 +1,18 @@
+// 프로젝트 구조
+
+src/
+|-- components/
+|   |-- UserList.vue
+|   |-- UserProfile.vue
+|   |-- UserPosts.vue
+|   |-- SinglePost.vue
+|
+|-- views/
+|   |-- Users.vue
+|   |-- User.vue
+|
+|-- routes/
+|   |-- index.ts
+|
+|-- App.vue
+|-- main.ts
