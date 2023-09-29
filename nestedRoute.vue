4978acdb405ffca8d98e2103c1c1664de2c8ca78
주정준 <65483521+jun1485@users.noreply.github.com>
2023-09-29 23:08:30 +0900

    Update nestedRoute.vue

--- nestedRoute.vue
+++ nestedRoute.vue
@@ -1,16 +1,6 @@
 // 프로젝트 구조
 
 src/
-|-- components/
-|   |-- UserList.vue
-|   |-- UserProfile.vue
-|   |-- UserPosts.vue
-|   |-- SinglePost.vue
-|
-|-- views/
-|   |-- Users.vue
-|   |-- User.vue
-|
 |-- routes/
 |   |-- index.ts
 |
