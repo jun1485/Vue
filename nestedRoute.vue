2f9d13fe1ee55cc256e77458bb0bed11f2dca4fb
주정준 <65483521+jun1485@users.noreply.github.com>
2023-10-01 22:24:14 +0900

    Update nestedRoute.vue

--- nestedRoute.vue
+++ nestedRoute.vue
@@ -1,8 +1,23 @@
 // 프로젝트 구조
 
 src/
+|-- components/
+|   |-- UserList.vue
+|   |-- UserProfile.vue
+|   |-- UserPosts.vue
+|   |-- SinglePost.vue
+|
+|-- views/
+|   |-- Users.vue
+|   |-- User.vue
+|
 |-- routes/
 |   |-- index.ts
 |
 |-- App.vue
 |-- main.ts
+
+// routes/index.ts
+import { createRouter, createWebHistory } from 'vue-router';
+import Users from '@/views/Users.vue';
+import User from '@/views/User.vue';
