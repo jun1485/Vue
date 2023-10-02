6bb69bb6fcd317bd37b7af0ce31d1619238a374e
주정준 <65483521+jun1485@users.noreply.github.com>
2023-10-02 21:57:48 +0900

    Update nestedRoute.vue

--- nestedRoute.vue
+++ nestedRoute.vue
@@ -21,3 +21,24 @@ src/
 import { createRouter, createWebHistory } from 'vue-router';
 import Users from '@/views/Users.vue';
 import User from '@/views/User.vue';
+
+const routes = [
+  {
+    path: '/users',
+    component: Users,
+    children: [
+      {
+        path: ':id',
+        component: User,
+        props: true,
+      }
+    ]
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
