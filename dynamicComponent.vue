a73adbc3626a0b95f4cff698b11fedf54b75d22d
주정준 <65483521+jun1485@users.noreply.github.com>
2023-11-21 09:28:29 +0900

    Create dynamicComponent.vue

---
+++ dynamicComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="currentComponent"></component>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const components = {
+  compA: defineAsyncComponent(() => import('./ComponentA.vue')),
+  compB: defineAsyncComponent(() => import('./ComponentB.vue'))
+};
+
+const currentComponent = ref('compA');
+
+// 컴포넌트를 변경하는 메소드 예제
+function switchComponent(componentName: string) {
+  currentComponent.value = componentName;
+}
+</script>
