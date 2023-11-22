3c6918b92d7ebad8256ffd4f8b46ac81a9a2d710
주정준 <65483521+jun1485@users.noreply.github.com>
2023-11-22 09:17:35 +0900

    Update dynamicComponent.vue

--- dynamicComponent.vue
+++ dynamicComponent.vue
@@ -1,3 +1,4 @@
+ // 하나의 마운트 포인트에서 여러 컴포넌트를 동적 교체
 <template>
   <component :is="currentComponent"></component>
 </template>
@@ -17,3 +18,7 @@ function switchComponent(componentName: string) {
   currentComponent.value = componentName;
 }
 </script>
+
+
+// 비동기 컴포넌트는 Vue의 'defineAsyncComponent' 메서드를 사용하여 정의.
+// 컴포넌트가 필요할 떄만 서버로부터 로드하도록 하여 초기 로드 시간을 줄여줌.
