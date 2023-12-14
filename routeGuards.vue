08ee3c1c7aaf806d25815569a09b0409b4e2b641
주정준 <65483521+jun1485@users.noreply.github.com>
2023-12-15 00:55:22 +0900

    Update routeGuards.vue

--- routeGuards.vue
+++ routeGuards.vue
@@ -1,3 +1,12 @@
 // 라우트 가드는 라우팅 프로세스 제어를 위한 기능.
 // 라우트 가드를 사용하면 특정 라우트로의 이동을 제한하거나, 라우트 이동 전/후에 특정 로직 실행 가능.
 // 권한 검사, 로그인 상태확인, 데이터 사전 로드 등에 사용.
+
+// 전역 가드
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !isAuthenticated()) {
+    next({ path: '/login' });
+  } else {
+    next();
+  }
+});
