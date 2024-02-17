8a33ef8bd300783e505cf7bfb1b9a2016a8cee76
주정준 <65483521+jun1485@users.noreply.github.com>
2024-02-17 22:42:00 +0900

    Create accessorProperty.vue

---
+++ accessorProperty.vue
@@ -0,0 +1,15 @@
+const person = {
+  firstName: 'Ju',
+  lastName: 'Jeong Jun',
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  set fullName(name) {
+    [this.firstName, this.lastName] = name.split(' ');
+  }
+};
+
+console.log(person.fullName); // 'Ju Jeong Jun'
+
+person.fullName = 'Elvin';
+console.log(person.fullName); // 'Elvin'
