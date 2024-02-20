ca1692c64577a3a3ecfe8cb2f097176dbed3d36f
주정준 <65483521+jun1485@users.noreply.github.com>
2024-02-20 22:56:28 +0900

    Update accessorProperty.vue

--- accessorProperty.vue
+++ accessorProperty.vue
@@ -1,3 +1,6 @@
+// get과 set은 객체의 속성에 접근하거나 값을 설정하는데 사용하는 접근자 프로퍼티.
+// 객체 내부의 속성을 보호, 속성에 접근하거나 설정할 때 추가적인 로직 실행 가능.
+
 const person = {
   firstName: 'Ju',
   lastName: 'Jeong Jun',
@@ -13,3 +16,17 @@ console.log(person.fullName); // 'Ju Jeong Jun'
 
 person.fullName = 'Elvin';
 console.log(person.fullName); // 'Elvin'
+
+
+const user = {
+  internalAge: 30,
+  get age() {
+    return this.internalAge;
+  },
+  set age(value) {
+    if (value < 0) {
+      throw new Error('나이는 음수가 될 수 없습니다.');
+    }
+    this.internalAge = value;
+  }
+};
