e0207a9f2c1325d44dde5dad25174105c0ba4ce0
주정준 <65483521+jun1485@users.noreply.github.com>
2024-03-10 23:03:23 +0900

    Update watch.vue

--- watch.vue
+++ watch.vue
@@ -23,7 +23,7 @@ watch: {
 }
 
 
-// computed는 주로 데이터를 파생시키는 데 사용, watch는 데이터의 변화에 따라 부수 효과를 처리하는 데 사용.
+// watch는 데이터의 변화에 따라 부수 효과를 처리하는 데 사용, computed는 주로 데이터를 파생시키는 데 사용
 // 데이터 변화에 따른 로직이 복잡하거나, 비동기 작업을 수행해야 하는 경우 watch 사용이 적합.
 
 // 비동기 작업과 함께 사용
@@ -34,5 +34,11 @@ const searchQuery = ref('');
 watch(() => searchQuery.value, async newValue => {
   const result = await fetch(`https://api.example.com/search?q=${newValue}`);
   console.log(await result.json());
-}, { immediate: false });
+}, { immediate: false }); 
 
+
+// immediate: true시 컴포넌트가 mount 될 때 지정한 반응형 소스가 처음으로 watch를 시작할 때, 즉시 콜백 함수를 실행.
+watch(() => selectedUserId, async (newId) => {
+  const userData = await fetchUserData(newId);
+  console.log(userData);
+}, { immediate: true });
