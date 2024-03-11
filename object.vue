6c866cb21788eefa34ee545314e5fa2dd2f5cfce
주정준 <65483521+jun1485@users.noreply.github.com>
2024-03-11 23:22:04 +0900

    Create object.vue

---
+++ object.vue
@@ -0,0 +1,10 @@
+// 구조분해 할당 -> 객체의 속성을 변수로 할당 가능.
+// 여러 값을 반환할 때 유용.
+const user = {
+  name: 'Elvin Ju',
+  age: 30
+};
+
+const { name, age } = user;
+console.log(name); // Elvin Ju
+console.log(age); // 28
