49191703d291d6603277685566ef7d514c0af790
주정준 <65483521+jun1485@users.noreply.github.com>
2024-03-24 23:50:48 +0900

    Update v-model.vue

--- v-model.vue
+++ v-model.vue
@@ -1,13 +1,37 @@
-<!-- ChildComponent.vue -->
+<!-- ParantComponent.vue -->
 <template>
-  <input
-    :value="value"
-    @input="$emit('input', $event.target.value)"
-  >
+  <ChildComponent v-model="data" />
+  <p>자식 컴포넌트에서의 입력값: {{ data }}</p>
 </template>
 
-<script>
-export default {
-  props: ['value']
+<script setup lang="ts">
+import { ref } from "vue";
+import ChildComponent from "./ChildComponent.vue";
+
+const data = ref("");
+</script>
+
+<style lang="scss"></style>
+
+
+<!-- ChildComponent.vue -->
+<template>
+  <div>
+    <input :value="modelValue" @input="onInput" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineEmits } from "vue";
+import { ref } from "vue";
+
+const emit = defineEmits(["update:modelValue"]);
+const modelValue = ref("");
+
+const onInput = (e: Event) => {
+  modelValue.value = (e.target as HTMLInputElement).value;
+  emit("update:modelValue", modelValue.value);
 };
 </script>
+
+<style lang="scss"></style>
