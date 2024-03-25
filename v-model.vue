9e5ceb6f4af8e13027661de8bf9a7c02b9bf1b15
주정준 <65483521+jun1485@users.noreply.github.com>
2024-03-25 22:59:49 +0900

    Update v-model.vue

--- v-model.vue
+++ v-model.vue
@@ -35,3 +35,37 @@ const onInput = (e: Event) => {
 </script>
 
 <style lang="scss"></style>
+
+
+// defineModel()에 첫번째 인수로 문자열을 전달하여 전달 가능.
+<!-- ParantComponent.vue -->
+<template>
+  <div>
+    <ChildComponent v-model:title="modelValue" />
+    자식으로부터의 데이터: {{ modelValue }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import ChildComponent from "./ChildComponent.vue";
+
+const modelValue = ref();
+</script>
+
+<style lang="scss"></style>
+
+
+<!-- ChildComponent.vue -->
+<template>
+  <div>
+    <input v-model="modelValue" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineModel } from "vue";
+const modelValue = defineModel("title");
+</script>
+
+<style lang="scss"></style>
