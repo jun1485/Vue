5256a28b129d812975297242b9cc078490db5dc1
주정준 <65483521+jun1485@users.noreply.github.com>
2024-04-01 23:30:46 +0900

    Create tanstackQuery.vue

---
+++ tanstackQuery.vue
@@ -0,0 +1,6 @@
+// main.ts
+import { createApp } from "vue";
+import App from "./App.vue";
+import { VueQueryPlugin } from "@tanstack/vue-query";
+
+createApp(App).use(VueQueryPlugin).mount("#app");
