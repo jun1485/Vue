5fe161ad9abb45c892d50f3aaad82b7f5ff4b25e
주정준 <65483521+jun1485@users.noreply.github.com>
2024-04-06 23:22:11 +0900

    Update example-query.vue

--- tanstack/example-query.vue
+++ tanstack/example-query.vue
@@ -2,21 +2,15 @@
   <div>
     <span v-if="isPending">Loading...</span>
     <span v-if="isError">{{ error?.message }}</span>
-
     <ul v-else>
-      <li v-for="todo in data" :key="todo.id">
-        {{ todo.title }}
-      </li>
+      <li v-for="todo in data" :key="todo.id">{{ todo.title }}</li>
       <button @click="onButtonClick">Add Todo</button>
     </ul>
   </div>
 </template>
-
 <script setup lang="ts">
 import { useMutation, useQuery, useQueryClient } from "@tanstack/vue-query";
-
 const queryClient = useQueryClient();
-
 const { isPending, data, error, isError } = useQuery({
   queryKey: ["todos"],
   queryFn: async () => {
@@ -24,15 +18,17 @@ const { isPending, data, error, isError } = useQuery({
     return response.json();
   },
 });
-
-const mutation = useMutation({
+const mutation = useMutation<
+  { title: string; completed: boolean },
+  unknown,
+  Error,
+  unknown
+>({
   mutationFn: async (newTodo) => {
     const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
       method: "POST",
       body: JSON.stringify(newTodo),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
+      headers: { "Content-type": "application/json; charset=UTF-8" },
     });
     return response.json();
   },
@@ -40,12 +36,8 @@ const mutation = useMutation({
     queryClient.invalidateQueries({ queryKey: ["todos"] });
   },
 });
-
 const onButtonClick = () => {
   mutation.mutate({ title: "New Todo", completed: false });
 };
 </script>
-
 <style lang="scss"></style>
-
-edit to place ...
