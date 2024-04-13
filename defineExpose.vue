237dcd047ca81b3e00e378c9b74f7d193c085c09
주정준 <65483521+jun1485@users.noreply.github.com>
2024-04-13 23:53:57 +0900

    Create defineExpose.vue

---
+++ defineExpose.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+// increment 함수와 count 변수를 외부에서 접근 가능하게 만듭니다.
+defineExpose({ increment, count })
+</script>
