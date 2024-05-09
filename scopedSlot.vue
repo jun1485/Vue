7ddf554477d8b859af03d7664f001507cb38359f
주정준 <65483521+jun1485@users.noreply.github.com>
2024-05-09 23:42:57 +0900

    Update scopedSlot.vue

--- scopedSlot.vue
+++ scopedSlot.vue
@@ -1,2 +1,5 @@
-// 명명된 슬롯에 'data'객체를 전달. 여기서 'data'는 부모 컴포넌트에서 정의된 데이터를 받는 매개변수.
-<template #title="data" />
+// 부모 컴포넌트는 슬롯을 정의할 때 특정 데이터를 하위 컴포넌트의 슬롯에 전달 가능.
+// 이 데이터는 슬롯을 사용하는 템플릿 내에서만 접근 가능.
+
+// #append-icon="data"는 명명된 슬롯(append-icon)에 data 객체를 전달
+<template #append-icon="data">
