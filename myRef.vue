af9527c8589178ac87286a7e0f0786374de39f08
주정준 <65483521+jun1485@users.noreply.github.com>
2024-05-21 23:08:10 +0900

    Create myRef.vue

---
+++ myRef.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+function ref(initValue) {
+  let _value = initValue;
+
+  const reactiveObject = {
+    get value() {
+      return _value;
+    },
+  };
+}
+</script>
+
+<template>
+  <div></div>
+</template>
